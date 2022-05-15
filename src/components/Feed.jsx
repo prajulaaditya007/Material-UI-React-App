c029d351d5d82235d0e1eef43afa34c213df6b82
prajulaaditya007 <en17el301100@medicaps.ac.in>
2022-05-15 23:48:33 +0530

    new changes

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,59 @@
 import React from "react";
-import { Box } from "@mui/material";
+import {
+    Avatar,
+    Box,
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    CardMedia, Checkbox,
+    IconButton,
+    Typography
+} from "@mui/material";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ShareIcon from '@mui/icons-material/Share';
+import {Favorite, FavoriteBorder} from "@mui/icons-material";
 
 const Feed = () => {
-  return (
-    <Box bgcolor="pink" flex={4} p={2}>
-      Feed
-    </Box>
-  );
+    return (
+        <Box flex={4} p={2}>
+            <Card>
+                <CardHeader
+                    avatar={
+                        <Avatar sx={{bgcolor: "red"}} aria-label="recipe">
+                            R
+                        </Avatar>
+                    }
+                    action={
+                        <IconButton aria-label="settings">
+                            <MoreVertIcon/>
+                        </IconButton>
+                    }
+                    title="Night Sky in Mountain Ranges"
+                    subheader="September 14, 2016"
+                />
+                <CardMedia
+                    component="img"
+                    // height="20%"
+                    image="https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    alt="mountains"
+                />
+                <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                        Lovely snowy mountains in the middle of the night, the starry night.
+                    </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <IconButton aria-label="add to favorites">
+                        <Checkbox icon={<FavoriteBorder/>} checkedIcon={<Favorite sx={{color: "#ff471a"}}/>}/>
+                    </IconButton>
+                    <IconButton aria-label="share">
+                        <ShareIcon/>
+                    </IconButton>
+                </CardActions>
+            </Card>
+        </Box>
+    );
 };
 
 export default Feed;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,92 @@
 import React from "react";
-import { Box } from "@mui/material";
+import {
+    Box,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText, Switch,
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import ArticleIcon from "@mui/icons-material/Article";
+import GroupsIcon from "@mui/icons-material/Groups";
+import StoreIcon from "@mui/icons-material/Store";
+import PeopleIcon from "@mui/icons-material/People";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 const Sidebar = () => {
-  return (
-    <Box
-      bgcolor="skyblue"
-      flex={1}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
-      Sidebar
-    </Box>
-  );
+    return (
+        <Box flex={1} p={2} sx={{display: {xs: "none", sm: "block"}}}>
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                        <ListItemIcon>
+                            <HomeIcon/>
+                        </ListItemIcon>
+                        <ListItemText primary="Homepage"/>
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#pages">
+                        <ListItemIcon>
+                            <ArticleIcon/>
+                        </ListItemIcon>
+                        <ListItemText primary="Pages"/>
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#group">
+                        <ListItemIcon>
+                            <GroupsIcon/>
+                        </ListItemIcon>
+                        <ListItemText primary="Groups"/>
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#market">
+                        <ListItemIcon>
+                            <StoreIcon/>
+                        </ListItemIcon>
+                        <ListItemText primary="Marketplace"/>
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#friends">
+                        <ListItemIcon>
+                            <PeopleIcon/>
+                        </ListItemIcon>
+                        <ListItemText primary="Friends"/>
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#settings">
+                        <ListItemIcon>
+                            <SettingsIcon/>
+                        </ListItemIcon>
+                        <ListItemText primary="Settings"/>
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#profile">
+                        <ListItemIcon>
+                            <AccountBoxIcon/>
+                        </ListItemIcon>
+                        <ListItemText primary="Profile"/>
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#profile">
+                        <ListItemIcon>
+                            <DarkModeIcon/>
+                        </ListItemIcon>
+                        <Switch></Switch>
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
+    );
 };
 
 export default Sidebar;
